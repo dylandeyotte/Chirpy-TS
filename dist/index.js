3f226d59ec818bbcfe797505f5ac5656db5fac66
dylandeyotte <dylan091deyotte@gmail.com>
2026-04-30 20:23:25 -0700

    index and readiness

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,12 @@
 import express from "express";
 const app = express();
-app.listen(8080);
+const PORT = 8080;
+app.use("/app", express.static("./src/app"));
+app.get("/healthz", handlerReadiness);
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
+async function handlerReadiness(req, res) {
+    res.set("Content-Type", "text/plain");
+    res.send("OK");
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
 import express from "express";
+import { handlerReadiness } from "./api/handlers.js";
 
 const app = express();
-app.listen(8080);
+const PORT = 8080;
+
+app.use("/app", express.static("./src/app"));
+
+app.get("/healthz", handlerReadiness);
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
